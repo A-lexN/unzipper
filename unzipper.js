0600faafb27f97dea4fae6d5cc4bce72506372fd
Alex N <120993746+bd1bx@users.noreply.github.com>
2023-03-01 16:03:51 +0700

    Add files via upload

---
+++ unzipper.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const AdmZip = require('adm-zip');
+
+// Set the path to the folder containing the archive files
+const archiveFolder = './folder/with/zip/files';
+
+// Set the path to the folder where you want to extract the archives
+const extractFolder = './folder/with/just/files';
+
+// Get an array of all the files in the archive folder
+const archiveFiles = fs.readdirSync(archiveFolder);
+
+// Loop through each file in the archive folder
+for (const archiveFile of archiveFiles) {
+  // Get the name of the folder to extract to (by removing the file extension)
+  const extractName = archiveFile.split('.').slice(0, -1).join('.');
+
+  // Create the folder to extract to (if it doesn't already exist)
+  fs.mkdirSync(`${extractFolder}/${extractName}`, { recursive: true });
+
+  // Extract the archive to the new folder
+  const zip = new AdmZip(`${archiveFolder}/${archiveFile}`);
+  zip.extractAllTo(`${extractFolder}/${extractName}`, true);
+}
